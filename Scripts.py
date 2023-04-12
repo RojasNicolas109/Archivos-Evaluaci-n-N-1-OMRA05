12972862664473dc6a45cff92fb72d115d760b0d
RojasNicolas109 <nicolas.rojas109@inacapmail.cl>
2023-04-12 15:01:57 +0000

    Evaluación N°1 OMRA05

---
+++ Scripts.py
@@ -0,0 +1,7 @@
+firstName = input("Cual es tu nombre?")
+lastaName = input("Cual es tu apellido?")
+section = input("Cual es tu Seccion?")
+sede = input("En que sede estudias?")
+print(firstName, lastaName, section + sede)
+
+    
---
+++ Test1.py
@@ -0,0 +1,1 @@
+print("Evaluación N°1 OMRA05 Nicolas Rojas Baez")
